2d8f8b813422cff09f23fd84512796a5b38c93a6
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-06-22 21:38:49 +0300

    color theme

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,18 +3,34 @@ import 'pages/login_page.dart';
 import 'components/bottom_nav_bar.dart';
 
 void main() {
-  runApp(const Melodiq());
+  runApp(MyMusicApp());
 }
 
-class Melodiq extends StatelessWidget {
-  const Melodiq({Key? key}) : super(key: key);
+class MyMusicApp extends StatelessWidget {
+  // Define your custom colors
+  final Color backgroundColor = const Color(0xFFDBC5AB);
+  final Color fontColor = const Color(0xFF583E23);
+  final Color buttonColor = const Color(0xFFFFF0E0);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'melodIQ',
+      title: 'My Music App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // Set the default background color
+        scaffoldBackgroundColor: backgroundColor,
+        // Define the default font color
+        // Define the default button theme
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: buttonColor, // Background color of the button
+            foregroundColor: Colors.white,
+            // Text color of the button
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: backgroundColor, // AppBar background color
+        ),
       ),
       initialRoute: '/login',
       routes: {
